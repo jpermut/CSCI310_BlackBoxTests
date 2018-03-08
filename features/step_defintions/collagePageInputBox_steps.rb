049126d9d3c6e57aab3894745d0816aea540bfed
Isaac Taylor <isaactay@usc.edu>
2018-03-07 20:39:57 -0800

    lalala

--- features/step_defintions/collagePageInputBox_steps.rb
+++ features/step_defintions/collagePageInputBox_steps.rb
@@ -7,3 +7,6 @@ end
 Then(/^I should see only "([^"]*)" in the search box$/) do |arg1|
   expect(page).to have_field(EnterTopicField, with: "p")
 end
+
+When(/^press enter$/) do 
+find_field("").native.send_key(:enter)
